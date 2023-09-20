bc728568a29cb0177252c4f2b902a1ecca21bc78
Park-Seung-Kwon <aortp@naver.com>
2023-09-20 19:04:59 +0900

    컨플릭수정

--- dasom/src/main/java/com/example/dasom/domain/vo/DonateWriteVo.java
+++ dasom/src/main/java/com/example/dasom/domain/vo/DonateWriteVo.java
@@ -12,7 +12,7 @@ import java.util.Date;
 public class DonateWriteVo {
 //    private Long donatewriteNumber;
 //    private Long adminNumber;
-    private String donatewriteTitle;
+    private String donateWriteTitle;
 //    private String donatewriteStatus;
 //    private Date donatewriteDate;
 //    private Long donatefileNumber;
