4d47d5ba981276d0c426e7b456f3a7d8a0cc4d09
JinHyukKang <jhkang0330@gmail.com>
2023-09-25 17:18:29 +0900

    강진혁 0925 1718 승권님 pull전 커밋

--- dasom/src/main/java/com/example/dasom/controller/MainPageController.java
+++ dasom/src/main/java/com/example/dasom/controller/MainPageController.java
@@ -1,23 +1,27 @@
 package com.example.dasom.controller;
 
+import com.example.dasom.service.MainPageService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/main/*")
+@RequiredArgsConstructor
 public class MainPageController {
 
+    private final MainPageService mainPageService;
+
     @GetMapping("/mainPage")
-    public String main(){
+    public String main(Model model){
+
+        model.addAttribute("donateList", mainPageService.findDonateAll());
+        model.addAttribute("csList", mainPageService.findCsAll());
         return "mainPage";
 
     }
 
-    @GetMapping("/mainPageLogin")
-    public String mainLogin(){
-        return "mainPageLogin";
-
-    }
 
 }
